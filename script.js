e990386ea445b25f044b0242e6f53908b22fb0fa
sksunil0608 <sksunil0608@gmail.com>
2023-11-02 01:48:37 +0530

    Updated Cart and Table View

--- script.js
+++ script.js
@@ -1,129 +1,256 @@
-
 form.addEventListener('submit',addProduct)
 form = document.getElementById('form');
 getProduct()
+async function getCurrentTotalCartValue() {
+  try {
+    const response = await axios.get(
+      "https://crudcrud.com/api/c680c2ec467648ccb8cc7fd3712330b4/product-data"
+    );
+    let totalCartValue = 0;
+    let totalCartItems = 0;
 
-function showProductOnPage(response){
+    if (response.data.length !== 0) {
+        totalCartItems = response.data.length;
+        totalCartValue = response.data.reduce(
+        (accumulator, currentValue) => accumulator + parseInt(currentValue.selling_price),
+        0
+      );
+    }
+    let totalCartValueElement = document.createElement("span");
+    totalCartValueElement.id = "total-cart-value";
+    totalCartValueElement.textContent = `-${totalCartValue}`;
+
+    let cartElement = document.getElementById("cart-value");
+    cartElement.appendChild(totalCartValueElement);
+
+    let totalCartItemElement = document.createElement("span");
+    totalCartItemElement.id = "total-cart-item";
+    totalCartItemElement.textContent = `-${totalCartItems}`;
+    let cartItemElement = document.getElementById("cart")
+    cartItemElement.appendChild(totalCartItemElement);
+
+
+
+  } catch (error) {
+    console.log("Error fetching current totalCartValue:", error);
+  }
+}
+function updateCurrentTotalCartValueOnPage() {
+  let totalCartValueElement = document.getElementById("total-cart-value");
+  totalCartValueElement.remove();
+
+  let totalCartItemElement = document.getElementById("total-cart-item");
+  totalCartItemElement.remove();
+
+
+  getCurrentTotalCartValue();
+}
+
+function showAllProductsOnPage(response){
     
     for(let i = 0;i<response.data.length;i++){
         let li = document.createElement('li');
     let detail = document.createElement('div');
         detail.id = response.data[i]._id
-        let p = document.createElement('p')
+        let div = document.createElement('div')
+        detail.appendChild(div);
+        
         for(key in response.data[i]){
-            p.innerHTML = `<strong>Product Name</strong>:${response.data[i].prodcut_name}
-            <strong>Selling Price:</strong>${response.data[i].selling_price}
-            <strong>Product Category:</strong>${response.data[i].product_category}`
+            div.innerHTML = `<table style="width: 90%; padding: 6px; margin: 30px;">
+                      <tr class="border-1">
+                        <th class="border-1">Product Name</th>
+                        <th class="border-1">Selling Price</th>
+                        <th class="border-1">Product Category</th>
+                        <th class="border-1">Delete</th>
+                        <th class="border-1">Update</th>
+                      </tr>
+                      <tr class="border-1">
+                        <td class="border-1">${response.data[i].product_name}</td>
+                        <td class="border-1">${response.data[i].selling_price}</td>
+                        <td class="border-1">${response.data[i].product_category}</td>
+                        <td class="border-1">
+                            <button class="btn btn-danger btn-sm input-group-text m-1" onclick="deleteProduct('${response.data[i]._id}')">
+                                Delete
+                            </button>
+                        </td>
+                        <td class="border-1">
+                            <button class="btn btn-warning btn-sm input-group-text m-1" onclick="updateProduct('${response.data[i]._id}')">
+                                Edit
+                            </button>
+                        </td>
+                      </tr>
+                   </table>`;
+                   
     }
-    detail.appendChild(p)
-    let delete_btn = document.createElement('button');
-    delete_btn.value = response.data[i]._id
-    delete_btn.textContent = "Delete"
-    delete_btn.addEventListener('click',function(event) {
-        event.preventDefault();
-        deleteProduct(delete_btn.value);
-    });
-    let edit_btn = document.createElement('button')
-    edit_btn.value = response.data[i]._id
-    edit_btn.textContent = "Edit"
-    edit_btn.addEventListener('click',function(event) {
-        event.preventDefault();
-        updateProduct(edit_btn.value);
-    });
     
-    detail.appendChild(delete_btn)
-    detail.appendChild(edit_btn)
+    
     li.appendChild(detail)
     ul = document.querySelector(`#${response.data[i].product_category}`)
     ul.appendChild(detail)
 }
 }
-async function getProduct(){
-    axios.get('https://crudcrud.com/api/ad61d9023c68461fbdde4983a1037277/product-data')
-    .then((response)=>{
-        showProductOnPage(response)
-    }).catch((err)=>{
-        console.log(err)
-    })
+
+function emptyAllInputBox(){
+    document.querySelector("#product-name").value = '';
+    document.querySelector("#selling-price").value = '';
+    document.querySelector("#product-category").value = '';
+
+    if(document.querySelector("#update-button")){
+        document.querySelector("#update-button").remove();
+    }
+
+}
+
+function showAddedProduct(response){
+    
+    let detail = document.createElement('div');
+    detail.id = response.data._id;
+    let div = document.createElement('div');
+    for(key in response.data){
+          div.innerHTML = `<table style="width: 90%; padding: 6px; margin: 30px;">
+                      <tr class="border-1">
+                        <th class="border-1">Product Name</th>
+                        <th class="border-1">Selling Price</th>
+                        <th class="border-1">Product Category</th>
+                        <th class="border-1">Delete</th>
+                        <th class="border-1">Update</th>
+                      </tr>
+                      <tr class="border-1">
+                        <td class="border-1">${response.data.product_name}</td>
+                        <td class="border-1">${response.data.selling_price}</td>
+                        <td class="border-1">${response.data.product_category}</td>
+                        <td class="border-1">
+                            <button class="btn btn-danger btn-sm input-group-text m-1" onclick="deleteProduct('${response.data._id}')">
+                                Delete
+                            </button>
+                        </td>
+                        <td class="border-1">
+                            <button class="btn btn-warning btn-sm input-group-text m-1" onclick="updateProduct('${response.data._id}')">
+                                Edit
+                            </button>
+                        </td>
+                      </tr>
+                   </table>`;
+    }
+    detail.appendChild(div)
+    ul = document.querySelector(`#${response.data.product_category}`);
+    ul.appendChild(detail);
+    
+}
+
+function showEditedProduct(response){
+    document.getElementById(`${response.data._id}`).remove();
+    showAddedProduct(response);
+}
+
+function showInputDataOnEditpage(data) {
+  document.querySelector("#product-name").value = data.product_name;
+  document.querySelector("#selling-price").value = data.selling_price;
+  document.querySelector("#product-category").value = data.product_category;
+}
+
+
+//-----------------------------------API Calling Starts Here--------------------------------//
+
+
+function getProduct(){
+    axios
+      .get(
+        "https://crudcrud.com/api/c680c2ec467648ccb8cc7fd3712330b4/product-data"
+      )
+      .then((response) => {
+        showAllProductsOnPage(response);
+        getCurrentTotalCartValue();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 }
 
 async function addProduct(event){
     event.preventDefault();
 
-    const prodcut_name = event.target.querySelector('#product-name').value;
+    const product_name = event.target.querySelector('#product-name').value;
     const selling_price = event.target.querySelector('#selling-price').value;
     const product_category = event.target.querySelector('#product-category').value;
     const obj = {
-        prodcut_name,
+        product_name,
         selling_price,
         product_category
     }
-    axios.post('https://crudcrud.com/api/ad61d9023c68461fbdde4983a1037277/product-data',obj).
-    then((response)=>{
-        location.reload();
-    }).catch((err)=>{
-        console.log(err)
-    })
+    try{
+        const response = await axios.post('https://crudcrud.com/api/c680c2ec467648ccb8cc7fd3712330b4/product-data',obj)
+        updateCurrentTotalCartValueOnPage();
+        showAddedProduct(response);
+        emptyAllInputBox();
+    }
+    catch(error){
+        console.log("Error Occured:",error)
+    }
+    
+    // axios
+    //   .post("https://crudcrud.com/api/c680c2ec467648ccb8cc7fd3712330b4/product-data", obj)
+    //   .then((response) => {
+    //     location.reload();
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
 }
-
 
 
 function deleteOnPage(id){
     let del_elem = document.getElementById(id)
     del_elem.remove();
 }
-async function deleteProduct(id){
+function deleteProduct(id){
     
-    axios.delete(`https://crudcrud.com/api/ad61d9023c68461fbdde4983a1037277/product-data/${id}`)
+    axios.delete(`https://crudcrud.com/api/c680c2ec467648ccb8cc7fd3712330b4/product-data/${id}`)
     .then((response)=>{
         deleteOnPage(id);
+        updateCurrentTotalCartValueOnPage();
     }).catch((err)=>{
         console.log(err)
     })
 }
 
-function showEditpage(data){
 
-        document.querySelector('#product-name').value = data.prodcut_name
-        document.querySelector('#selling-price').value = data.selling_price
-        document.querySelector('#product-category').value = data.product_category
-
-
-    }
-
-async function updateProduct(id){
-    await axios(`https://crudcrud.com/api/ad61d9023c68461fbdde4983a1037277/product-data/${id}`)
+function updateProduct(id){
+    axios(`https://crudcrud.com/api/c680c2ec467648ccb8cc7fd3712330b4/product-data/${id}`)
     .then((response)=>{
-        showEditpage(response.data)
+        showInputDataOnEditpage(response.data)
     }).catch((err)=>{
         console.log(err)
     })
-    let update = document.createElement('button')
-    update.textContent="update"
-    document.querySelector('#form-div').appendChild(update)
-
-
+    let updateBtn = document.createElement('button')
+    updateBtn.textContent="update"
+    updateBtn.id = "update-button"
+    document.querySelector('#form-div').appendChild(updateBtn)
     
-    update.addEventListener('click',putData)
+    updateBtn.addEventListener('click',putData)
 
     async function putData(event){
         event.preventDefault()
-        const prodcut_name = document.querySelector('#product-name').value;
+        const product_name = document.querySelector('#product-name').value;
         const selling_price = document.querySelector('#selling-price').value;
         const product_category = document.querySelector('#product-category').value;
 
         const obj ={
-            prodcut_name,
+            product_name,
             selling_price,
             product_category
         }
 
-        await axios.put(`https://crudcrud.com/api/ad61d9023c68461fbdde4983a1037277/product-data/${id}`,obj)
-        .then((res)=>{
-            location.reload();
-        }).catch((err)=>{
-            console.log(err)
-        })
+        try{
+            await axios.put(`https://crudcrud.com/api/c680c2ec467648ccb8cc7fd3712330b4/product-data/${id}`,obj)
+            const response = await axios.get(`https://crudcrud.com/api/c680c2ec467648ccb8cc7fd3712330b4/product-data/${id}`);
+            showEditedProduct(response);
+            updateCurrentTotalCartValueOnPage();
+            emptyAllInputBox();
+        }
+        catch(error){
+            console.log(error)
+        }
     }
 
 }
